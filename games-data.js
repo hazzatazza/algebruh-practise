44065d5f873dfd1614ca87840a9ece9c45f0aae1
hazzatazza <gdharry123@gmail.com>
2026-03-26 22:44:37 +1100

    Update and rename backend.json to games-data.js

---
+++ games-data.js
@@ -0,0 +1,17 @@
+// Save this as games-data.js in your GitHub repo
+window.allGames = [
+  { 
+    id: "sw", 
+    title: "Space Waves", 
+    url: "https://raw.githack.com/bubbls/UGS-Assets/main/space%20waves/index.html", 
+    thumb: "spacewaves.png" 
+  },
+  { 
+    id: "ssg", 
+    title: "Super Stickman Golf", 
+    url: "https://raw.githack.com/hsinidev/stickman-golf-unblocked/master/index.html", 
+    thumb: "stickmangolf.png" 
+  },
+  // You can just keep pasting your 100+ games here. 
+  // If you miss one quote, the whole site won't necessarily die.
+];
